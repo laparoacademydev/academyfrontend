57bea65657621de3796473b024cdda24886c9930
laparoacademydev <academy@laparosimulators.com>
2022-01-13 12:00:23 +0100

    First Attempts at making separate App.js work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
 import GetCourses from "./academycontentstorage/laparoacademy-jsoncontent/courses.json";
 
-const courses = GetCourses;
-
 function App() {
-  return <h1>Waiting</h1>;
+  const [courses, setCourses] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  React.useEffect(() => {
+    if (loaded === false) {
+      checkUserActive();
+    }
+  });
+
+  function checkUserActive() {
+    if (courses === null) {
+      setCourses(GetCourses);
+      setLoaded(true);
+    }
+  }
+
+  console.log(courses);
+
+  if (loaded === false) {
+    return <h1>Waiting</h1>;
+  } else if (courses === null) {
+    return <h1>Waiting</h1>;
+  } else {
+    return <h1>Loaded</h1>;
+  }
 }
 
 export default App;
