ba8091f0439ad7b8a104385a0270c01e985d55c9
laparoacademydev <academy@laparosimulators.com>
2022-01-13 11:01:41 +0100

    app.js and first getcourses temp attempt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import GetCourses from "./academycontentstorage/laparoacademy-jsoncontent/courses.json";
+
+const courses = GetCourses;
+
 function App() {
   return <h1>Waiting</h1>;
 }
