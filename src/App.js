c60a77c71b3bdf6a00deefb6a3aeebd5290ffb57
laparoacademydev <academy@laparosimulators.com>
2022-01-14 09:39:39 +0100

    axios testhttprequest and setcourseidscenariolist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
 
-import GetCourses from "./academycontentstorage/laparoacademy-jsoncontent/courses.json";
+// import GetCourses from "./academycontentstorage/laparoacademy-jsoncontent/courses.json";
+
+export class AppHelper {
+  static ApiUrl =
+    "https://academylaparomanagementservice.azure-api.net/laparoacademyfunctionapp/";
+}
 
 function App() {
   const [courses, setCourses] = useState(null);
   const [loaded, setLoaded] = useState(false);
+
+  const [selectedTraining, setSelectedTraining] = useState(null);
+  const [trainingList, setTrainingList] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [playingScenario, setPlayingScenario] = useState(false);
+  const [selectedCourseID, setSelectedCourseID] = useState(null);
 
   React.useEffect(() => {
     if (loaded === false) {
@@ -13,14 +25,39 @@ function App() {
     }
   });
 
+  function getCourses() {
+    if (courses === null) {
+      axios.get(`${AppHelper.ApiUrl}TestTrigger1`).then((response) => {
+        setCourses(response.data);
+        setCourseIdAndScenarioList(response.data.courses[0]);
+      });
+    }
+  }
+
   function checkUserActive() {
     if (courses === null) {
-      setCourses(GetCourses);
+      getCourses();
       setLoaded(true);
     }
   }
 
-  console.log(courses);
+  function setCourseIdAndScenarioList(selectedCourse) {
+    setSelectedTraining(null);
+    setTrainingList(null);
+    setSelectedItem(null);
+    setPlayingScenario(false);
+    setSelectedCourseID(selectedCourse.id);
+
+    let scenarioFileNames = [];
+    let scenarioTypes = [];
+    selectedCourse.content.forEach((x) => {
+      scenarioTypes.push(x.type);
+      scenarioFileNames.push(x.id + ".json");
+    });
+
+    console.log(scenarioFileNames);
+    console.log(scenarioTypes);
+  }
 
   if (loaded === false) {
     return <h1>Waiting</h1>;
