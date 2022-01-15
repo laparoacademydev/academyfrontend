07a4c45bafb70185ad8fe7daad93497b00ca4832
laparoacademydev <academy@laparosimulators.com>
2022-01-15 17:30:01 +0100

    almost working - one props undefined

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
+import { useState, Fragment } from "react";
+
+import ScenarioList from "./components/ScenarioList/ScenarioList";
+import DisplayedItemContent from "./components/DisplayedItemContent/DisplayedItemContent";
+import WebcamTraining from "./components/WebcamTraining/WebcamTraining";
+import TrainingsList from "./components/TrainingsList/TrainingsList";
+import ShowTraining from "./components/ShowTraining/ShowTraining";
+import AccessCodeScreen from "./components/AccessCodeScreen/AccessCodeScreen";
+// import decodeJWT from "jwt-decode";
+import Layout from "./components/UI/Layout";
 
 // import GetCourses from "./academycontentstorage/laparoacademy-jsoncontent/courses.json";
 
-// export class AppHelper {
-//   static ApiUrl =
-//     "https://academylaparomanagementservice.azure-api.net/laparoacademyfunctionapp/";
-// }
+export class AppHelper {
+  static ApiUrl =
+    "https://academylaparomanagementservice.azure-api.net/laparoacademyfunctionapp/";
+  static storageUrl = "./academycontentstorage/";
+}
 
 function App() {
   const [courses, setCourses] = useState(null);
   const [loaded, setLoaded] = useState(false);
-
+  const [userIsActive, setUserIsActive] = useState(0);
   const [selectedTraining, setSelectedTraining] = useState(null);
   const [trainingList, setTrainingList] = useState(null);
   const [selectedItem, setSelectedItem] = useState(null);
   const [playingScenario, setPlayingScenario] = useState(false);
   const [selectedCourseID, setSelectedCourseID] = useState(null);
   const [selectedScenarioList, setSelectedScenarioList] = useState(null);
+  const [userPanelActive, setUserPanelActive] = useState(0);
 
   React.useEffect(() => {
     if (loaded === false) {
@@ -27,15 +38,14 @@ function App() {
   });
 
   function checkUserActive() {
-    if (courses === null) {
-      getCourses();
-      setLoaded(true);
-    }
+    getCourses();
+    setLoaded(true);
+    setUserIsActive("user is active?");
   }
 
   function getCourses() {
     if (courses === null) {
-      fetch("./academycontentstorage/laparoacademy-jsoncontent/courses.json", {
+      fetch(`${AppHelper.storageUrl}laparoacademy-jsoncontent/courses.json`, {
         headers: {
           "Content-Type": "application/json",
 
@@ -52,14 +62,6 @@ function App() {
           setCourseIdAndScenarioList(myJson.courses[0]);
         });
     }
-
-    // if (courses === null) {
-    //   axios.get(`${AppHelper.ApiUrl}TestTriggerGetCourses`).then((response) => {
-    //     setCourses(response.data);
-    //     // console.log(response.data);
-    //     setCourseIdAndScenarioList(response.data.courses[0]);
-    //   });
-    // }
   }
 
   function setCourseIdAndScenarioList(selectedCourse) {
@@ -109,15 +111,137 @@ function App() {
     });
   }
 
+  function getUserEmail() {
+    return "test@test.com";
+  }
+
+  // function requestSelectedTraining(training) {
+  //   // this is for managing existing trainings
+  // }
+
+  function setSelectedCourseItem(selected) {
+    setSelectedScenarioList(null);
+    setSelectedItem(selected);
+  }
+
+  function requestSetTrainingList() {
+    setSelectedTraining(null);
+    setSelectedCourseID(null);
+    setTrainingList("?");
+  }
+
+  function deleteTraining(training) {
+    // nothing happens
+  }
+
   // console.log(scenarioFileNames);
   // console.log(scenarioTypes.length);
 
   if (loaded === false) {
-    return <h1>Waiting</h1>;
+    return <h1>Waiting Loaded False</h1>;
+  } else if (userIsActive === 0) {
+    return <h1>Waiting Access Code Screen</h1>;
+    // <AccessCodeScreen sendAccessCode={sendAccessCode} />;
   } else if (courses === null) {
     return <h1>Waiting</h1>;
+  } else if (selectedTraining !== null) {
+    return (
+      <Fragment>
+        <Layout
+          items={courses}
+          setCourseIdAndScenarioList={setCourseIdAndScenarioList}
+          selectedCourseID={selectedCourseID}
+          userEmail={getUserEmail()}
+          setTrainingList={requestSetTrainingList}
+          userIsActive={userIsActive}
+          // url={AppHelper.ApiUrl}
+          userPanelActive={userPanelActive}
+          setUserPanelActive={setUserPanelActive}
+        />
+        <ShowTraining training={selectedTraining}></ShowTraining>
+      </Fragment>
+    );
+  } else if (trainingList !== null) {
+    return (
+      <Fragment>
+        <Layout
+          items={courses}
+          setCourseIdAndScenarioList={setCourseIdAndScenarioList}
+          selectedCourseID={selectedCourseID}
+          userEmail={getUserEmail()}
+          setTrainingList={requestSetTrainingList}
+          userIsActive={userIsActive}
+          // url={AppHelper.ApiUrl}
+          userPanelActive={userPanelActive}
+          setUserPanelActive={setUserPanelActive}
+        />
+        <TrainingsList
+          trainingList={trainingList}
+          // setSelectedTraining={requestSelectedTraining}
+          deleteTraining={deleteTraining}
+        ></TrainingsList>
+      </Fragment>
+    );
+  } else if (selectedItem === null) {
+    return (
+      <Fragment>
+        <Layout
+          items={courses}
+          setCourseIdAndScenarioList={setCourseIdAndScenarioList}
+          selectedCourseID={selectedCourseID}
+          userEmail={getUserEmail()}
+          setTrainingList={requestSetTrainingList}
+          userIsActive={userIsActive}
+          // url={AppHelper.ApiUrl}
+          userPanelActive={userPanelActive}
+          setUserPanelActive={setUserPanelActive}
+        />
+        <ScenarioList
+          selectedScenarioList={selectedScenarioList}
+          setSelectedCourseItem={setSelectedCourseItem}
+        ></ScenarioList>
+      </Fragment>
+    );
+  } else if (playingScenario === false) {
+    return (
+      <Fragment>
+        <Layout
+          items={courses}
+          setCourseIdAndScenarioList={setCourseIdAndScenarioList}
+          selectedCourseID={selectedCourseID}
+          userEmail={getUserEmail()}
+          setTrainingList={requestSetTrainingList}
+          userIsActive={userIsActive}
+          // url={AppHelper.ApiUrl}
+          userPanelActive={userPanelActive}
+          setUserPanelActive={setUserPanelActive}
+        />
+        <DisplayedItemContent
+          selectedItemContent={selectedItem}
+          setPlayingScenario={setPlayingScenario}
+        ></DisplayedItemContent>
+      </Fragment>
+    );
   } else {
-    return <h1>Loaded</h1>;
+    return (
+      <Fragment>
+        <Layout
+          items={courses}
+          setCourseIdAndScenarioList={setCourseIdAndScenarioList}
+          selectedCourseID={selectedCourseID}
+          userEmail={getUserEmail()}
+          setTrainingList={requestSetTrainingList}
+          userIsActive={userIsActive}
+          // url={AppHelper.ApiUrl}
+          userPanelActive={userPanelActive}
+          setUserPanelActive={setUserPanelActive}
+        />
+        <WebcamTraining
+          // url={AppHelper.ApiUrl}
+          name={selectedItem.name.en}
+        />
+      </Fragment>
+    );
   }
 }
 
