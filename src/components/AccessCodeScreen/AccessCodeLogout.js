c02a271a6610b3220eb61941027c001f70c9e144
laparoacademydev <academy@laparosimulators.com>
2022-04-07 11:27:18 +0200

    ACD-23 Submit/Logout Button

--- src/components/AccessCodeScreen/AccessCodeLogout.js
+++ src/components/AccessCodeScreen/AccessCodeLogout.js
@@ -6,12 +6,12 @@ function AccessCodeLogout() {
   return (
     <div
       className={`${buttonclasses.button}  ${classes.accesscodelogoutbtn}`}
-      onClick={function () {
+      onClick={() => {
         window.localStorage.removeItem("jwt");
         window.location.href = `${AppHelper.LoginUrl}`;
       }}
     >
-      <div className={`${classes.accesscodesubmitbtntext}`}>Log Out</div>
+      <div className={`${classes.logoutbtntext}`}>Log Out</div>
     </div>
   );
 }
