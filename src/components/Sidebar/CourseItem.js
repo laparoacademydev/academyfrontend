8d609c564a344770357ce09a68547ef5309e43d0
laparoacademydev <academy@laparosimulators.com>
2022-08-26 15:06:37 +0200

    ACD-87 1B GreenLine course list

--- src/components/Sidebar/CourseItem.js
+++ src/components/Sidebar/CourseItem.js
@@ -1,17 +1,33 @@
 import classes from "./Sidebar.module.css";
 
 function CourseItem(props) {
+  function CheckIfSelected() {
+    if (props.course.id === props.selectedCourseID) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   return (
-    <div
-      className={
-        props.course.id === props.selectedCourseID
-          ? classes.courseitemselected
-          : classes.courseitem
-      }
-      onClick={() => props.setCourseIdAndScenarioList(props.course)}
-    >
-      <div className={classes.courseitemdescription}>
-        {props.course.name[props.selectedLanguage]}
+    <div>
+      <div
+        className={
+          CheckIfSelected()
+            ? classes.courseitemgreenline
+            : classes.courseitemnoline
+        }
+      ></div>
+
+      <div
+        className={
+          CheckIfSelected() ? classes.courseitemselected : classes.courseitem
+        }
+        onClick={() => props.setCourseIdAndScenarioList(props.course)}
+      >
+        <div className={classes.courseitemdescription}>
+          {props.course.name[props.selectedLanguage]}
+        </div>
       </div>
     </div>
   );
