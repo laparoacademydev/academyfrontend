b32c00ef1173f269d644671d0af8e20e285b7e5d
laparoacademydev <academy@laparosimulators.com>
2022-09-23 13:16:53 +0200

    ACD-110 Access Code Sticker in Access Code Screen

--- src/components/AccessCodeScreen/AccessCodeScreen.js
+++ src/components/AccessCodeScreen/AccessCodeScreen.js
@@ -2,6 +2,8 @@ import classes from "./AccessCodeScreen.module.css";
 import React, { useState } from "react";
 import AccessCodeLogout from "./AccessCodeLogout";
 import AccessCodeSubmit from "./AccessCodeSubmit";
+import { Fragment } from "react";
+import LaparoSticker from "./LaparoSticker";
 
 function AccessCodeScreen(props) {
   const [error, setError] = useState(undefined);
@@ -11,29 +13,32 @@ function AccessCodeScreen(props) {
   }
 
   return (
-    <div className={classes.accesscodebackground}>
-      <div className={classes.accesscodewindow}>
-        <div className={classes.accesscodetext}>
-          {error === undefined
-            ? "Please submit the serial number of your Laparo device. You will find it on the device. If you can't find it, please contact us at academy@laparosimulators.com"
-            : error}
-        </div>
-        <AccessCodeSubmit
-          sendAccessCode={props.sendAccessCode}
-          accessCodeError={props.accessCodeError}
-          code={code}
-          handleChange={handleChange}
-          setCode={setCode}
-        />
-
-        <AccessCodeLogout />
-        {props.accessCodeError ? (
-          <div className={classes.erroraccesscodetext}>
-            Sorry, your access code is not valid.
+    <Fragment>
+      <LaparoSticker></LaparoSticker>
+      <div className={classes.accesscodebackground}>
+        <div className={classes.accesscodewindow}>
+          <div className={classes.accesscodetext}>
+            {error === undefined
+              ? "Please submit the product number of your Laparo device. See example below. If you can't find it, please contact us at academy@laparosimulators.com"
+              : error}
           </div>
-        ) : null}
+          <AccessCodeSubmit
+            sendAccessCode={props.sendAccessCode}
+            accessCodeError={props.accessCodeError}
+            code={code}
+            handleChange={handleChange}
+            setCode={setCode}
+          />
+
+          <AccessCodeLogout />
+          {props.accessCodeError ? (
+            <div className={classes.erroraccesscodetext}>
+              Sorry, your access code is not valid.
+            </div>
+          ) : null}
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/AccessCodeScreen/LaparoSticker.js
@@ -0,0 +1,349 @@
+import React from "react";
+import classes from "./LaparoSticker.module.css";
+
+function LaparoSticker() {
+  function Icon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="399"
+        height="269"
+        version="1.1"
+        viewBox="0 0 399 269"
+        xmlSpace="preserve"
+      >
+        <g>
+          <g display="inline" transform="translate(84.435 162.272)">
+            <g transform="matrix(1 0 0 .51147 0 33.728)">
+              <path d="M20 0H22V100H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .51147 4 33.728)">
+              <path d="M20 0H22V100H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 8 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 12 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 14 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 22 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 24 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 32 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 38 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 44 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 46 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 50 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 56 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 60 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 66 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 68 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 74 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 82 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 84 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 88 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .51147 96 33.728)">
+              <path d="M20 0H22V100H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .63924 100 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .63924 102 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 108 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 110 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 114 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 116 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 122 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 124 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 128 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 130 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 132 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 136 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 142 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 152 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 156 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 166 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 170 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 178 33.728)">
+              <path d="M20 0H22V80H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .60262 184 33.728)">
+              <path strokeWidth="0.921" d="M20 0H22V84.875H20z"></path>
+            </g>
+            <g transform="matrix(1 0 0 .51147 188 33.728)">
+              <path d="M20 0H22V100H20z"></path>
+            </g>
+            <g>
+              <text
+                x="38"
+                y="100"
+                style={{
+                  InkscapeFontSpecification: "'sans-serif, Normal'",
+                  fontVariantLigatures: "normal",
+                  fontVariantCaps: "normal",
+                  fontVariantNumeric: "normal",
+                  fontVariantEastAsian: "normal",
+                }}
+                display="inline"
+                fontFamily="sans-serif"
+                fontSize="24"
+                fontStretch="normal"
+                fontStyle="normal"
+                fontVariant="normal"
+                fontWeight="normal"
+              >
+                <tspan x="38" y="100">
+                  12345
+                </tspan>
+              </text>
+            </g>
+            <g>
+              <text
+                x="128"
+                y="100"
+                style={{
+                  InkscapeFontSpecification: "'sans-serif, Normal'",
+                  fontVariantLigatures: "normal",
+                  fontVariantCaps: "normal",
+                  fontVariantNumeric: "normal",
+                  fontVariantEastAsian: "normal",
+                }}
+                display="inline"
+                fontFamily="sans-serif"
+                fontSize="24"
+                fontStretch="normal"
+                fontStyle="normal"
+                fontVariant="normal"
+                fontWeight="normal"
+              >
+                <tspan x="128" y="100">
+                  67890
+                </tspan>
+              </text>
+            </g>
+          </g>
+          <text
+            xmlSpace="preserve"
+            style={{
+              InkscapeFontSpecification: "'Arial, Normal'",
+              fontVariantLigatures: "normal",
+              fontVariantCaps: "normal",
+              fontVariantNumeric: "normal",
+              fontVariantEastAsian: "normal",
+              WebkitTextAlign: "justify",
+              textAlign: "justify",
+            }}
+            x="264.911"
+            y="193.499"
+            fill="#000"
+            fillOpacity="1"
+            strokeDasharray="none"
+            strokeWidth="1.757"
+            display="inline"
+            fontFamily="Arial"
+            fontSize="14.667"
+            fontStretch="normal"
+            fontStyle="normal"
+            fontVariant="normal"
+            fontWeight="normal"
+          >
+            <tspan x="264.911" y="193.499">
+              MADE IN EU
+            </tspan>
+          </text>
+          <text
+            xmlSpace="preserve"
+            style={{
+              lineHeight: "1.5",
+              InkscapeFontSpecification: "'Arial, Normal'",
+              fontVariantLigatures: "normal",
+              fontVariantCaps: "normal",
+              fontVariantNumeric: "normal",
+              fontVariantEastAsian: "normal",
+              WebkitTextAlign: "justify",
+              textAlign: "justify",
+            }}
+            x="9.625"
+            y="101.453"
+            fill="#000"
+            fillOpacity="1"
+            strokeDasharray="none"
+            strokeWidth="1.757"
+            display="inline"
+            fontFamily="Arial"
+            fontSize="14.667"
+            fontStretch="normal"
+            fontStyle="normal"
+            fontVariant="normal"
+            fontWeight="normal"
+            wordSpacing="1"
+          >
+            <tspan x="9.625" y="101.453">
+              Type: Aspire
+            </tspan>
+            <tspan x="9.625" y="123.453">
+              Prod. No: A******
+            </tspan>
+            <tspan x="9.625" y="145.453">
+              Manufacturer:
+            </tspan>
+            <tspan x="9.625" y="167.453">
+              Laparo Sp. z o.o.
+            </tspan>
+            <tspan x="9.625" y="189.453">
+              laparosimulators.com
+            </tspan>
+          </text>
+          <text
+            xmlSpace="preserve"
+            style={{
+              InkscapeFontSpecification: "'Arial, Normal'",
+              fontVariantLigatures: "normal",
+              fontVariantCaps: "normal",
+              fontVariantNumeric: "normal",
+              fontVariantEastAsian: "normal",
+              WebkitTextAlign: "justify",
+              textAlign: "justify",
+            }}
+            x="138.026"
+            y="100.499"
+            fill="#000"
+            fillOpacity="1"
+            strokeDasharray="none"
+            strokeWidth="1.757"
+            fontFamily="Arial"
+            fontSize="14.667"
+            fontStretch="normal"
+            fontStyle="normal"
+            fontVariant="normal"
+            fontWeight="normal"
+          >
+            <tspan x="138.026" y="100.499">
+              Input:
+            </tspan>
+            <tspan x="138.026" y="118.832">
+              2x USB 5V{" "}
+            </tspan>
+            <tspan x="138.026" y="137.165">
+              Camera 500 mA
+            </tspan>
+            <tspan x="138.026" y="155.499">
+              Light 350 mA
+            </tspan>
+          </text>
+          <g
+            fill="#000"
+            fillRule="evenodd"
+            clipRule="evenodd"
+            transform="translate(301.75 92.5) scale(.2875)"
+          >
+            <path d="M110 199.499a100 100 0 01-10 .501 100 100 0 010-200 100 100 0 0110 .501v30A70 70 0 00100 30a70 70 0 000 140 70 70 0 0010-.501z"></path>
+            <path d="M280 199.499a100 100 0 01-10 .501 100 100 0 010-200 100 100 0 0110 .501v30A70 70 0 00270 30a70 70 0 00-68.38 55H260v30h-58.38A70 70 0 00270 170a70 70 0 0010-.501z"></path>
+          </g>
+          <rect
+            width="120.915"
+            height="22.274"
+            x="5.657"
+            y="107.127"
+            fill="#000"
+            fillOpacity="0"
+            stroke="red"
+            strokeDasharray="none"
+            strokeOpacity="1"
+            strokeWidth="1.757"
+            ry="10.687"
+          ></rect>
+        </g>
+        <g
+          fill="#01629c"
+          fillOpacity="1"
+          fillRule="nonzero"
+          transform="translate(9.97 12.684) scale(.19685)"
+        >
+          <path d="M.332 14.2v135.445L369.18 11.48zm0 0"></path>
+          <path d="M369.18 163.188V18.137L36.29 164.078zm0 0"></path>
+          <path d="M0 193.656V356.98l369.18-51.94h-.32V170.89zm0 0"></path>
+          <path d="M486.887 11.48h-47.032v289.317H592.04V258.71H486.887zm0 0"></path>
+          <path d="M765.281 106.621c1.848 5.465 3.61 10.316 5.328 14.535l29.664 65.996H719.63l29.637-65.996a244.069 244.069 0 005.437-15.12c1.774-5.462 3.461-11.384 5.024-17.684 1.863 6.707 3.703 12.804 5.554 18.27m-146.05 194.175h49.394l33.61-74.512h114.624l32.801 74.512h49.606L759.34.02zm0 0"></path>
+          <path d="M1037.809 123.953c-8.7 5.64-26.098 8.488-52.157 8.488H973.57V51.81h12.082c24.864 0 41.996 3.152 51.27 9.476 9.3 6.305 13.926 17.203 13.926 32.606 0 14.359-4.348 24.37-13.04 30.062m61.477-32.832c0-13.695-2.57-26.25-7.73-37.637-5.117-11.382-12.262-20.457-21.332-27.187-7.383-5.238-16.047-9.047-25.996-11.363-9.922-2.286-25.45-3.454-46.555-3.454h-71.133v289.317h44.082V170.965h27.05c21.106 0 36.634-1.117 46.556-3.356 9.949-2.234 18.613-6.02 25.996-11.257 9.07-6.606 16.215-15.704 21.332-27.293 5.16-11.559 7.73-24.243 7.73-37.938"></path>
+          <path d="M1226.559 106.621c1.843 5.465 3.62 10.316 5.336 14.535l29.644 65.996h-80.644l29.652-65.996c1.832-4.597 3.648-9.656 5.441-15.12 1.778-5.462 3.457-11.384 5.031-17.684 1.844 6.707 3.684 12.804 5.54 18.27M1080.5 300.796h49.414l33.598-74.512h114.633l32.793 74.512h49.617L1220.625.02zm0 0"></path>
+          <path d="M1434.871 134.805V51.809h8.488c23.723 0 39.95 3.254 48.715 9.78 8.77 6.509 13.153 17.766 13.153 33.7 0 14.762-4.106 25.004-12.262 30.824-8.168 5.793-24.711 8.692-49.606 8.692zm101.773 10.773c11.184-13.113 16.801-31.23 16.801-54.457 0-13.187-2.437-25.309-7.316-36.34-4.867-11.078-11.781-20.101-20.766-27.09-7.48-5.82-16.554-9.96-27.164-12.449-10.61-2.515-25.527-3.762-44.765-3.762h-65.614v289.317h44.067V171.344h5.347l96.25 129.453h53.149L1487.03 169.21c21.895-2.64 38.41-10.547 49.614-23.633"></path>
+          <path d="M1862.988 198.129c-5.324 13.113-13.023 24.805-23.023 35.094-10.164 10.265-21.574 18.12-34.266 23.507-12.746 5.387-26.152 8.106-40.242 8.106-14.36 0-27.926-2.719-40.723-8.106-12.77-5.386-24.18-13.242-34.18-23.507-10.011-10.29-17.66-21.93-22.933-34.993-5.285-13.035-7.89-26.882-7.89-41.496 0-14.765 2.605-28.714 7.89-41.906 5.273-13.164 12.922-24.828 22.934-34.988 10-10.242 21.449-18.121 34.304-23.508 12.82-5.387 26.356-8.105 40.598-8.105 14.207 0 27.773 2.742 40.594 8.207 12.86 5.488 24.156 13.289 33.914 23.406 10 10.289 17.7 22.031 23.023 35.191 5.336 13.164 7.993 27.067 7.993 41.703 0 14.485-2.657 28.309-7.993 41.395m45.438-99.59c-7.703-18.394-18.918-34.914-33.66-49.5-14.754-14.36-31.485-25.285-50.11-32.809-18.652-7.52-38.386-11.253-59.199-11.253-20.965 0-40.785 3.734-59.477 11.253-18.726 7.524-35.437 18.45-50.199 32.81-14.77 14.483-25.972 30.901-33.62 49.222-7.626 18.297-11.462 37.789-11.462 58.472 0 20.813 3.836 40.328 11.461 58.5 7.649 18.168 18.852 34.457 33.621 48.84 14.48 14.207 31.168 25.188 50.086 32.883 18.918 7.7 38.754 11.54 59.59 11.54 20.559 0 40.188-3.79 58.879-11.435 18.727-7.652 35.512-18.68 50.43-32.988 14.742-14.383 25.957-30.722 33.66-49.047 7.738-18.293 11.574-37.734 11.574-58.293 0-20.43-3.836-39.82-11.574-58.195"></path>
+          <path d="M1902.059 15.094h1.41c1.64 0 2.96-.547 2.96-1.875 0-1.168-.863-1.95-2.73-1.95-.777 0-1.324.075-1.64.153zm-.074 7.258h-2.961V9.555c1.168-.235 2.804-.395 4.902-.395 2.43 0 3.52.395 4.445.942.715.539 1.258 1.554 1.258 2.808 0 1.403-1.09 2.496-2.652 2.957v.16c1.242.47 1.941 1.407 2.34 3.121.39 1.95.62 2.73.937 3.204h-3.203c-.39-.473-.621-1.641-1.016-3.13-.23-1.402-1.015-2.023-2.656-2.023h-1.394zm-7.88-6.72c0 5.7 4.208 10.223 9.985 10.223 5.617 0 9.746-4.523 9.746-10.14 0-5.695-4.129-10.297-9.82-10.297-5.703 0-9.91 4.602-9.91 10.215m23.011 0c0 7.262-5.695 12.953-13.101 12.953-7.344 0-13.188-5.691-13.188-12.953 0-7.094 5.844-12.793 13.188-12.793 7.406 0 13.101 5.7 13.101 12.793"></path>
+        </g>
+      </svg>
+    );
+  }
+
+  return (
+    <div className={classes.laparosticker}>
+      <Icon></Icon>
+    </div>
+  );
+}
+
+export default LaparoSticker;
