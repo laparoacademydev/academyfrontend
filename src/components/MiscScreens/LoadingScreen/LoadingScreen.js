c5e502ffa5622880c375d029b103abfbf446748e
zmgrsk <zkmgrsk@scyzoryk.xyz>
2023-02-27 11:33:53 +0100

    CSS Spinner Bug Fix and package-lock updates

--- src/components/MiscScreens/LoadingScreen/LoadingScreen.js
+++ src/components/MiscScreens/LoadingScreen/LoadingScreen.js
@@ -4,7 +4,7 @@ import classes from "./LoadingScreen.module.css";
 function LoadingScreen(props) {
   return (
     <div className={classes.loadingscreenbackground}>
-      <div className={classes.spinner}></div>
+      <div className={classes.spin}></div>
       <div className={classes.loadingscreentext}>Please wait ... </div>
       <div className={classes.loadingmsg}>{props.loadingScreenMsg}</div>
     </div>
